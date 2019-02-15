b3492770e064840e8d99ea97b7f5a7c1c6886662
Juanjo Arranz <juanjoarranzales@gmail.com>
2019-02-15 09:09:25 +0100

    Load items for the selected list

--- src/webparts/listItems/ListItemsWebPart.ts
+++ src/webparts/listItems/ListItemsWebPart.ts
@@ -47,12 +47,12 @@ export default class ListItemsWebPart extends BaseClientSideWebPart<IListItemsWe
 
     // reference to item dropdown needed later after selecting a list
     this.itemsDropDown = new PropertyPaneAsyncDropdown( 'item', {
-      label           : strings.ItemFieldLabel,
-      loadOptions     : this.loadItems.bind( this ),
+      label: strings.ItemFieldLabel,
+      loadOptions: this.loadItems.bind( this ),
       onPropertyChange: this.onListItemChange.bind( this ),
-      selectedKey     : this.properties.item,
+      selectedKey: this.properties.item,
       // should be disabled if no list has been selected
-      disabled        : !this.properties.listName
+      disabled: !this.properties.listName
     } );
 
     return {
@@ -103,10 +103,27 @@ export default class ListItemsWebPart extends BaseClientSideWebPart<IListItemsWe
 
   private onListChange( propertyPath: string, newValue: any ): void {
     const oldValue: any = get( this.properties, propertyPath );
+
     // store new value in web part properties
     update( this.properties, propertyPath, (): any => { return newValue; } );
+
+    // reset selected item
+    this.properties.item = undefined;
+
+    // store new value in web part properties
+    update( this.properties, 'item', (): any => { return this.properties.item; } );
+
     // refresh web part
     this.render();
+
+    // reset selected values in item dropdown
+    this.itemsDropDown.properties.selectedKey = this.properties.item;
+
+    // allow to load items
+    this.itemsDropDown.properties.disabled = false;
+
+    // load items and re-render items dropdown
+    this.itemsDropDown.render();
   }
 
   private loadItems(): Promise<IDropdownOption[]> {
